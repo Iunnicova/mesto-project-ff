cf629cc93865d38fdbc532c98554f55b9617bdaf
Iunnicova <iunnicova@gmail.com>
2024-05-21 14:25:06 +0500

    feat: add adding and removing cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const cardDeleteButton = document.querySelector('.card__delete-button');
 
-// @todo: DOM узлы
+initialCards.forEach(({ name, link }) => {
+	const listElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+	listElement.querySelector('.card__image').alt = name;
+	listElement.querySelector('.card__image').src = link;
+	listElement.querySelector('.card__title').textContent = name;
+	listElement.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+	placesList.append(listElement);
 
-// @todo: Функция удаления карточки
+	addEventListener('click', function (addAnEvent) {
+		const cardDeleteButton = addAnEvent.target.closest('.card');
+		cardDeleteButton.remove();
+	});
+});
 
-// @todo: Вывести карточки на страницу
+console.log(initialCards);
+
